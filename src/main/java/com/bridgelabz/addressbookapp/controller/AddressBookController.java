82ef5a467d397d7c531be362e55abfd2405f1b72
lokeshmk25 <lokeshmk2211@gmail.com>
2021-12-14 23:05:12 +0530

    Added: Get method of Http request

--- src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
@@ -1,12 +1,25 @@
 package com.bridgelabz.addressbookapp.controller;
 
+import com.bridgelabz.addressbookapp.dto.AddressDTO;
+import com.bridgelabz.addressbookapp.services.AddressBookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/addressbook")
 public class AddressBookController {
 
+    @Autowired
+    private AddressBookService addressBookService;
+
     @RequestMapping({"","/"})
     public String greetings(){
         return "Welcome to AddressBook application";
+    }
+
+    @RequestMapping("/getall")
+    public List<AddressDTO> getallAddress(){
+        return addressBookService.getallAddress();
     }
 }
--- src/main/java/com/bridgelabz/addressbookapp/services/AddressBookService.java
+++ src/main/java/com/bridgelabz/addressbookapp/services/AddressBookService.java
@@ -1,9 +1,13 @@
 package com.bridgelabz.addressbookapp.services;
 
+import com.bridgelabz.addressbookapp.dto.AddressDTO;
 import com.bridgelabz.addressbookapp.repository.AddressBookRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AddressBookService {
@@ -11,4 +15,12 @@ public class AddressBookService {
       private AddressBookRepository repository;
        @Autowired
       private ModelMapper modelMapper;
+
+       public List<AddressDTO> getallAddress(){
+           return repository.findAll().stream()
+                   .map(addressBookData -> {
+                       return modelMapper.map(addressBookData, AddressDTO.class);
+                   })
+                   .collect(Collectors.toList());
+       }
 }
