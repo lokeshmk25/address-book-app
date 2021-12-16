efe9b8526fdf3af4c8688a01a5487a2348ca98bc
lokeshmk25 <lokeshmk2211@gmail.com>
2021-12-16 19:36:12 +0530

    Added: Integration Test cases to getallAddress

--- src/test/java/com/bridgelabz/addressbookapp/integration/AddressBookIntegrationTest.java
+++ src/test/java/com/bridgelabz/addressbookapp/integration/AddressBookIntegrationTest.java
@@ -1,4 +1,38 @@
 package com.bridgelabz.addressbookapp.integration;
 
+import com.bridgelabz.addressbookapp.controller.AddressBookController;
+import com.bridgelabz.addressbookapp.dto.AddressDTO;
+import com.bridgelabz.addressbookapp.services.AddressBookService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest(AddressBookController.class)
 public class AddressBookIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private AddressBookService addressBookService;
+
+
+    @Test
+    void getAllAddressTest() throws Exception {
+        when(addressBookService.getallAddress()).thenReturn(new ArrayList<>());
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/addressbook/getall"))
+                .andExpect(status().isOk());
+    }
 }
